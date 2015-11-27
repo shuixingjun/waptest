88fb8e16f8e2bb6a707003b728f2ec25f7218589
shuixingjun <almoyeyo@gmail.com>
2015-11-27 22:50:46 +0800

    js

---
+++ js/login.js
@@ -0,0 +1,51 @@
+$(function(){
+
+
+	var normal = function(){
+        this.nextAll('.error').hide();
+      }
+      , wrong = function(text){
+        this.nextAll('.error').html(text).show();
+      }
+ 
+
+
+     $.validator.validType = {
+                username: {
+                    type: ['required', '请输入手机号'],
+                    trim: true, 
+                    valid:[
+                        ['regexp', /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/]
+                        //['ajax', 'url', '']
+                    ],
+                    text:['手机号码不正确','账号或密码不正确'],
+                    right:'',
+                    callback:function(){ 
+                    }
+                    },
+                password:{
+                    type: ['required', '请输入密码'],
+                    focus: '请输入密码',
+                     valid:[
+                        ['length', 6, 16]
+                    ],
+                    text:['6-16'],
+                    callback:function(){}
+                    }
+                };    
+        $.validator.validEvent = {
+         '#username': 'blur'  
+       ,'#password': 'blur'
+      };
+
+      $login_form = $.validator({
+        selector: '#login_form'
+        , normal: normal
+        , right: normal       
+        , wrong: wrong
+        }).on('submit', function(event) {//登录
+      event.preventDefault();
+      	console.log(111);
+          })
+
+});
